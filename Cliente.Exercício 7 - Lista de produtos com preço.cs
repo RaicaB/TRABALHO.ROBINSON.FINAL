6eba84d2a481038a45b9254a78af4fd67f9d289a
RaicaB <raicamartins97@gmail.com>
2026-06-20 20:10:48 -0300

    Add files via upload

---
+++ Cliente.Exercício 7 - Lista de produtos com preço.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public class Cliente
+{
+    public string Nome { get; set; }
+    public string Telefone { get; set; }
+    public string Cidade { get; set; }
+
+    public Cliente(string nome, string telefone, string cidade)
+    {
+        Nome = nome;
+        Telefone = telefone;
+        Cidade = cidade;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {Nome} | Telefone: {Telefone} | Cidade: {Cidade}";
+    }
+}
+
