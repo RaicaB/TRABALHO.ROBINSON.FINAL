bbb00d89535a5c3fc66c7cdaf1782924b5373481
RaicaB <raicamartins97@gmail.com>
2026-06-20 20:03:18 -0300

    Add files via upload

---
+++ Program.Exercício 6 - Lista de produtos com preço.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Produto> produtos = new List<Produto>();
+        double valorTotal = 0;
+
+        Console.WriteLine("=== Cadastro de Produtos ===");
+
+        for (int i = 1; i <= 4; i++)
+        {
+            Console.WriteLine($"\nProduto {i}:");
+
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o preço: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            produtos.Add(new Produto(nome, preco));
+            valorTotal += preco;
+        }
+
+        Console.WriteLine("\n--- Produtos cadastrados ---");
+        foreach (Produto p in produtos)
+        {
+            Console.WriteLine(p);
+        }
+
+        Console.WriteLine($"\nValor total: R$ {valorTotal:F2}");
+        Console.ReadKey();
+    }
+}
