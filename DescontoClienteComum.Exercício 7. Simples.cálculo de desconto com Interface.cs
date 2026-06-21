c32f47d4f1e2f425636465b724d314d6759ff0ca
RaicaB <raicamartins97@gmail.com>
2026-06-21 13:32:22 -0300

    Add files via upload

---
+++ DescontoClienteComum.Exercício 7. Simples.cálculo de desconto com Interface.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public class DescontoClienteComum : IDesconto
+{
+    public decimal Calcular(decimal valor)
+    {
+        return valor - (valor * 0.05m);
+    }
+}
